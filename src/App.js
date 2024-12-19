cdffa617b94dbf6e609e633b6d3b383329bd8651
Satyam <satyamkushwah016@gmail.com>
2024-12-19 14:00:40 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AppRoute from './route/appRoutes';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AppRoute from "./route/appRoutes";
+import logo from "./img/61020.jpg";
 function App() {
-  return (
-    <AppRoute/>
-  );
+    return (
+        <>
+            <head>
+                <title>Movie Review</title>
+                <link rel="icon" type="image/jpg" href={logo}/>
+            </head>
+            <AppRoute />
+        </>
+    );
 }
 
 export default App;
